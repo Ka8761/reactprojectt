41c1e2a013f1fb0e1e34abc285d1eb7498d40c27
Dikachi Nwankwo <dikachinwankwo575@gmail.com>
2024-07-04 01:48:46 +0100

    added navbar components

--- myapp/src/components/navbar/Navbar.jsx
+++ myapp/src/components/navbar/Navbar.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import './Navbar.css'
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import { TiThMenu } from "react-icons/ti";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
+  const toggleMenu = () =>{
+    setShow(!show)
+  }
   return (
-    <div>Navbar</div>
+    <>
+    <div className='nav'>
+    
+      <TiThMenu className='homeicon' onClick={toggleMenu}/>
+      <div className= {`navitems ${show? '': 'hide'}`}  >
+      <div className='item'>
+      <div>HOME PAGE</div>
+      <span className='shadow'></span>
+      </div>
+     
+       <div className='item'>
+      <div>ABOUT</div>
+      <span className='shadow'></span>
+      </div>
+
+      <div className='item'>
+      <div>FAQ</div>
+      <span className='shadow'></span>
+      </div>
+
+      <div className='item'>
+      <div>CONTACT US</div>
+      <span className='shadow'></span>
+      </div>
+
+      </div>
+
+      <div className='navbuttons'>
+        <button>REGISTER</button>
+        <button>LOGIN</button>
+      </div>
+    </div>
+    <div className='navline'></div>
+    </>
   )
 }
 
